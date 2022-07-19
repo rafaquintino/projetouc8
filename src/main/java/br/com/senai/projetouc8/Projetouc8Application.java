a60b26c605c9064b8b7935d647b53396cf5cb0d9
Rafael Quintino <rafaelquintino8115@gamil.com>
2022-07-18 22:12:17 -0300

    Versão 0.0.1 Projeto base

---
+++ src/main/java/br/com/senai/projetouc8/Projetouc8Application.java
@@ -0,0 +1,13 @@
+package br.com.senai.projetouc8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetouc8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetouc8Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senai/projetouc8/Projetouc8ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.projetouc8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetouc8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
